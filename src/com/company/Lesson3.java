cf38fe3eec00bd7265e5fa2203e4c23d3282d767
illia.berdnyk <illia.berdnyk@gmail.com>
2018-07-21 23:05:44 +0300

    this is third task

---
+++ src/com/company/Lesson3.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lesson3 {
+    private static int sum(int a, int b){
+        return (a+b);
+    }
+
+
+    public static void main (String[] args) {
+        System.out.println(sum(4,5));
+
+   }
+}
