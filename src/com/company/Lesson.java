4ade1d2237267777d7e76872c1ff3e652bae5545
illia.berdnyk <illia.berdnyk@gmail.com>
2018-07-21 22:31:58 +0300

    this is first task

---
+++ src/com/company/Lesson.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Lesson {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");//output the text in console
+	// write your code here
+    }
+}
