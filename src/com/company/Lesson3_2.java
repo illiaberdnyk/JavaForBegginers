03041c15af29f9199e19b9d60aedb38de683ca8d
illia.berdnyk <illia.berdnyk@gmail.com>
2018-07-23 23:05:47 +0300

    this is third(1) task

---
+++ src/com/company/Lesson3_2.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Lesson3_2 {
+    public static void main(String[]args){
+        System.out.println("Enter the three numbers:");
+
+        System.out.println("Enter number one: ");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter number two: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter number three: ");
+        int num3 = sc.nextInt();
+
+        if (num1>num2 & num1>num3){
+            System.out.println("Max number is"+num1);
+        }
+        else if (num2>num3 & num2>num1)
+            System.out.println("Max number is"+num2);
+        else System.out.println("Max number is"+num3);
+    }
+}
